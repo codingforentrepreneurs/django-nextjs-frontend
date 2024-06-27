0e54d97b38a23b7a2bd727919fc6a7cbd667214e
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-27 12:21:21 -0600

    9 - Your First API request from Next to Django

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
+"use client"
+
 import Image from "next/image";
 
 export default function Home() {
+  async function getDjangoAPIData() {
+    const response = await fetch("http://127.0.0.1:8001/api/hello");
+    const data = await response.json();
+    console.log(data);
+  }
+
+  async function handleClick() {
+    await getDjangoAPIData()
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button onClick={handleClick}>
+        Lookup Data
+      </button>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;

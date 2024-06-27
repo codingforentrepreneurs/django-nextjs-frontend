e3f7ff7389383ce33407ca8a24f9f26e9e187b8c
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-27 13:52:10 -0600

    14 - Data from Nextjs Server to Django Backend

--- src/app/api/login/route.jsx
+++ src/app/api/login/route.jsx
@@ -2,18 +2,37 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
+const DJANGO_API_LOGIN_URL = "http://127.0.0.1:8001/api/token/pair"
+
 export async function POST(request) {
-    const data = await request.json()
-    console.log(data)
+    const myAuthToken = cookies().get('auth-token')
+    console.log(myAuthToken.value)
+    
+    const requestData = await request.json()
+    const jsonData = JSON.stringify(requestData)
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: jsonData
+    }
+    const response = await fetch(DJANGO_API_LOGIN_URL, requestOptions)
+    const responseData = await response.json()
+    if (response.ok) {
+        console.log("logged in")
+        const authToken = responseData.access
+        cookies().set({
+            name: 'auth-token',
+            value: authToken,
+            httpOnly: true, // limit client-side js
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV !== 'development',
+            maxAge: 3600,
+        })
+    }
 
     // const authToken = cookies().get("auth-token")
-    cookies().set({
-        name: 'auth-token',
-        value: 'abc',
-        httpOnly: true, // limit client-side js
-        sameSite: 'strict',
-        secure: process.env.NODE_ENV !== 'development',
-        maxAge: 3600,
-    })
+   
     return NextResponse.json({"hello": "world"}, {status: 200})
 }   

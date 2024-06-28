eb57e3fb954f2359a8554238386029be5496d705
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-28 12:27:19 -0600

    23 - Navbar Component

---
+++ src/components/layout/NavLinks.jsx
@@ -0,0 +1,15 @@
+
+
+const NavLinks = [
+    {
+        label: "Dashboard",
+        authRequired: false,
+        href: "/"
+    },
+    {
+        label: "Waitlist",
+        authRequired: true,
+        href: "/waitlist"
+    }
+]
+export default NavLinks
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,118 @@
+"use client"
+
+import Link from "next/link"
+import { CircleUser, Menu, Package2, Search } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { useAuth } from "../authProvider"
+
+import NavLinks from './NavLinks'
+
+function BrandLink({displayName, className}){
+    const finalClass = className ? className : "flex items-center gap-2 text-lg font-semibold md:text-base"
+    return <Link
+        href="/"
+        className={finalClass}
+    >
+        <Package2 className="h-6 w-6" />
+        {displayName ? 
+             <span>SaaS</span>
+            : 
+            <span className="sr-only">SaaS</span>
+        }
+    </Link>
+}
+
+export default function Navbar({className}) {
+    const auth = useAuth()
+    const finalClass = className ? className : "sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6"
+    return  <header className={finalClass}>
+    <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <BrandLink displayName={true} />
+        {NavLinks.map((navLinkItem, idx)=>{
+            const shouldHide = !auth.isAuthenticated &&navLinkItem.authRequired
+
+            return shouldHide ? null : <Link
+                href={navLinkItem.href}
+                className="text-muted-foreground transition-colors hover:text-foreground"
+            >
+                {navLinkItem.label}
+            </Link>
+        })}
+    </nav>
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="shrink-0 md:hidden"
+        >
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Toggle navigation menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <nav className="grid gap-6 text-lg font-medium">
+          <BrandLink displayName={true} className="flex items-center gap-2 text-lg font-semibold" />
+          
+          {NavLinks.map((navLinkItem, idx)=>{
+            const shouldHide = !auth.isAuthenticated &&navLinkItem.authRequired
+
+            return shouldHide ? null : <Link
+                href={navLinkItem.href}
+                className="text-muted-foreground hover:text-foreground"
+            >
+                {navLinkItem.label}
+            </Link>
+        })}
+          
+          {auth.isAuthenticated && 
+          <Link href="/logout" className="text-muted-foreground hover:text-foreground">
+            Logout
+          </Link>
+          }
+        </nav>
+      </SheetContent>
+    </Sheet>
+    <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+      <form className="ml-auto flex-1 sm:flex-initial">
+        <div className="relative">
+          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            type="search"
+            placeholder="Search products..."
+            className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+          />
+        </div>
+      </form>
+      {auth.isAuthenticated &&
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+            </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    }
+    </div>
+  </header>
+}

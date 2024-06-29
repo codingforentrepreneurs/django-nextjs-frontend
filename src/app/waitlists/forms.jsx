2ce12ede9be8d9e18fff6be287e263ff47f3adbd
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-29 12:50:46 -0600

    34 - Rendering Django Form Validation Errors in Nextjs

--- src/app/waitlists/forms.jsx
+++ src/app/waitlists/forms.jsx
@@ -9,10 +9,13 @@ const WAITLIST_API_URL = "/api/waitlists/"
 
 export default function WaitlistForm() {
   const [message, setMessage] = useState('')
+  const [errors, setErrors] = useState({})
   const [error, setError] = useState('')
   async function handleSubmit (event) {
         event.preventDefault()
-        console.log(event, event.target)
+        setMessage('')
+        setErrors({})
+        setError('')
         const formData = new FormData(event.target)
         const objectFromForm = Object.fromEntries(formData)
         const jsonData = JSON.stringify(objectFromForm)
@@ -25,25 +28,37 @@ export default function WaitlistForm() {
         }
         const response = await fetch(WAITLIST_API_URL, requestOptions)
         // const data = await response.json()
-        if (response.ok) {
+        if (response.status === 201 || response.status === 200) {
             setMessage("Thank you for joining")
-  
         } else {
-            setError("There was an error with your request. Please try again")
+              const data = await response.json()
+              setErrors(data)
+              if (!data.email) {
+                setError("There was an error with your request. Please try again.")
+              }
         }
     }
-  return <form onSubmit={handleSubmit}>
-    <div>{message && message}</div>
-    <div>{error && error}</div>
+  return <form onSubmit={handleSubmit} className='space-y-4'>
+    {message && <div className='rounded-md bg-accent p-3 font-semibold text-sm'>{message}</div>}
+    {error &&<div className='rounded-md text-white bg-destructive p-3 font-semibold text-sm'>{error}</div>}
   <div className="grid gap-2">
     <Label htmlFor="email">Email</Label>
-    <Input
-      id="email"
-      type="email"
-      name="email"
-      placeholder="Your email"
-      required
-    />
+    <div className={errors?.email ? "rounded-lg p-3 border border-destructive" : ''}>
+      <Input
+        id="email"
+        type="email"
+        name="email"
+        placeholder="Your email"
+        required
+      />
+      {errors && errors?.email && errors?.email.length > 0 && <div className='p-1 text-sm bg-destructive text-center text-white'>
+          {errors?.email.map((err, idx)=>{
+            return !err.message ? null : <p key={`err-${idx}`}>
+                {err.message}
+            </p>
+          })}
+      </div>}
+    </div>
   </div>
   <Button type="submit" className="w-full">
     Join Waitlist

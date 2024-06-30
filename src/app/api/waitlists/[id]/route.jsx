6be6cffee0d1054667268edd5fa615e013548162
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-30 11:51:49 -0600

    41 - Dynamic Routing in Nextjs Pages and Routes

--- src/app/api/waitlists/[id]/route.jsx
+++ src/app/api/waitlists/[id]/route.jsx
@@ -8,11 +8,9 @@ const DJANGO_API_WAITLISTS_URL=`${DJANGO_API_ENDPOINT}/waitlists/`
 
 export async function GET(request, {params}) {
     const endpoint = params?.id ? `${DJANGO_API_WAITLISTS_URL}${params.id}/` : null
-    console.log('endpoint', endpoint, params)
     if (!endpoint){
         return NextResponse.json({}, {status: 400})
     }
-  
     const {data, status} = await ApiProxy.get(endpoint, true)
     return NextResponse.json(data, {status: status})
 }
